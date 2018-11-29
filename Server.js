8051e1bf10d0869aadb00b42d60518c289ce7c2f
ashish chide <ashishchide1101@gmail.com>
2018-11-29 22:56:17 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
+console.log("Listening on Port 5000");
 });
